6ef98279a27a85528f5a2019608d155acacc94d7
joni.i7 <jhoey.potter7@gmail.com>
2019-01-24 14:44:05 +0700

    Perubahan Config

--- apps/config/config.php
+++ apps/config/config.php
@@ -2,7 +2,7 @@
 $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['index_page'] = "services.php/";
+$config['index_page'] = "index.php/";
 $config['uri_protocol']	= "AUTO";
 $config['url_suffix'] = ".php";
 $config['language']	= "english";
