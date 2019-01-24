bb75d61cf52841bf7734945d3dbb2035580577aa
joni.i7 <jhoey.potter7@gmail.com>
2019-01-24 14:47:47 +0700

    Perubahan System

--- sys/helpers/function_helper.php
+++ sys/helpers/function_helper.php
@@ -1,73 +1,226 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  defined('BASEPATH') OR exit('No direct script access allowed');
 
-if(!function_exists('emptystr')){
-	function emptystr($str){
-		if(($str=="") || ($str=='null') || ($str=='-')  || ($str=='00000000'))
-			$return = "-";
-		else
-			$return = WhiteSpaceXML($str);
-		
-		$var_data = trim($return);
-		return $var_data;
-	}
-}
-
-if(!function_exists('escape')){	
-	function escape($text,$type){
-		if(!empty($text)){
-			if($type == "DATESLASH"){
-				$arrdate = explode("/",$text);
-				$text = $arrdate[2]."-".$arrdate[1]."-".$arrdate[0];
-			}else if($type == "DATESLASHS"){
-				$arrdate = explode("/",$text);
-				$text = $arrdate[2]."-".$arrdate[0]."-".$arrdate[1];
-			}else if($type == "DATEDASH"){
-				$text = substr($text,0,4)."-".substr($text,4,2)."-".substr($text,6,2);
-			}else if($type == "STRING"){
-				$text = "";
+if(!function_exists('format_terbilang'))
+{
+	function format_terbilang($num,$dec=4){
+		$stext = array(
+			"Nol",
+			"Satu",
+			"Dua",
+			"Tiga",
+			"Empat",
+			"Lima",
+			"Enam",
+			"Tujuh",
+			"Delapan",
+			"Sembilan",
+			"Sepuluh",
+			"Sebelas"
+		);
+		$say  = array(
+			"Ribu",
+			"Juta",
+			"Milyar",
+			"Triliun",
+			"Biliun" // remember limitation of float
+			
+		);
+		$w = "";
+	
+		if ($num <0 ) {
+			$w  = "Minus ";
+			//make positive
+			$num *= -1;
+		}
+	
+		$snum = number_format($num,$dec,",",".");
+	   // die($snum);
+		$strnum =  explode(".",substr($snum,0,strrpos($snum,",")));
+		//parse decimalnya
+		$koma = substr($snum,strrpos($snum,",")+1);
+	
+		$isone = substr($num,0,1)  ==1;
+		if (count($strnum)==1) {
+			$num = $strnum[0];
+			switch (strlen($num)) {
+				case 1:
+				case 2:
+					if (!isset($stext[$strnum[0]])){
+						if($num<20){
+							$w .=$stext[substr($num,1)]." Belas";
+						}else{
+							$w .= $stext[substr($num,0,1)]." Puluh ".
+								(intval(substr($num,1))==0 ? "" : $stext[substr($num,1)]);
+						}
+					}else{
+						$w .= $stext[$strnum[0]];
+					}
+					break;
+				case 3:
+					$w .=  ($isone ? "Seratus" : format_terbilang(substr($num,0,1)) .
+						" Ratus").
+						" ".(intval(substr($num,1))==0 ? "" : format_terbilang(substr($num,1)));
+					break;
+				case 4:
+					$w .=  ($isone ? "Seribu" : format_terbilang(substr($num,0,1)) .
+						" Ribu").
+						" ".(intval(substr($num,1))==0 ? "" : format_terbilang(substr($num,1)));
+					break;
+				default:
+					break;
 			}
-			$result = trim(strtoupper($text));
 		}else{
-			$result = null;
+			$text = $say[count($strnum)-2];
+			$w = ($isone && strlen($strnum[0])==1 && count($strnum) <=2? "Se".strtolower($text) : format_terbilang($strnum[0]).' '.$text);
+			array_shift($strnum);
+			$i =count($strnum)-2;
+			foreach ($strnum as $k=>$v) {
+				if (intval($v)) {
+					$w.= ' '.format_terbilang($v).' '.($i >=0 ? $say[$i] : "");
+				}
+				$i--;
+			}
 		}
-		return $result;
+		$w = trim($w);
+		if ($dec = intval($koma)) {
+			$w .= " koma ". format_terbilang($koma);
+		}
+		return trim($w);
 	}
 }
 
-if(!function_exists('WhiteSpaceXML')){	
-	function WhiteSpaceXML($text){
-		$hasil = str_replace("&","&amp;",$text);
-		$hasil = str_replace("'","&apos;",$hasil);
-		$hasil = str_replace("\"","&quot;",$hasil);
-		$hasil = str_replace("<","&lt;",$hasil);
-		$hasil = str_replace(">","&gt;",$hasil);
-		return trim($hasil);
-	}
-}
-
-if(!function_exists('check_file')){	
-	function check_file($filename){
-		if(file_exists($filename)){
-			return true;
+if(!function_exists('grant')){
+	function grant(){
+		$ci =& get_instance();
+		$ci->load->database();
+		$KD_USER = $ci->session->userdata('ID');
+		$KD_GROUP = $ci->session->userdata('KD_GROUP');
+		$segs_1 = $ci->uri->segment(1);
+		$segs_2 = $ci->uri->segment(2);
+		$arrsegs = explode('_',$segs_2);
+		if($segs_2!="") $furi = trim($segs_1."/".$arrsegs[0]);
+		$return = "";
+		if($KD_GROUP=="SPA"){
+			$return = 'W';
 		}else{
-			return false;
+			$query = "SELECT A.ID, B.HAK_AKSES
+					  FROM app_menu A
+					  INNER JOIN app_user_menu B ON B.KD_MENU=A.ID 
+					  WHERE B.KD_USER = ".$ci->db->escape($KD_USER)."
+					  AND A.URL LIKE ".$ci->db->escape('%'.$furi.'%');
+			$result = $ci->db->query($query); 
+			if($result->num_rows() > 0){
+				$akses = $result->row()->HAK_AKSES;
+				$return = $akses;
+			}
 		}
+		return $return;
 	}
 }
 
-if(!function_exists('create_file')){	
-	function create_file($filename){
-		$filename = fopen($filename, "w");
-		fputs($filename, $txt);
-		fclose($filename);
+if(!function_exists('str_xml')){
+	function str_xml($data){
+		if(strtoupper(trim($data))==""){
+        	$return = "";
+		}else{
+			$return = str_replace("&","&amp;",$data);
+			$return = str_replace("'","&apos;",$return);
+			$return = str_replace("\"","&quot;",$return);
+			$return = str_replace("<","&lt;",$return);
+			$return = str_replace(">","&gt;",$return);	
+			$return = trim($return);
+		}
+		return $return;
 	}
 }
 
-if(!function_exists('remove_file')){
-	function remove_file($filename) {
-		if(file_exists($filename)){
-			unlink($filename);
+if(!function_exists('validate')){
+	function validate($data, $type="TEXT"){
+		if(strtoupper(trim($data))==""){
+        	$return = NULL;
+		} else {
+			switch ($type) {
+				case "TEXT":
+					if (trim($data) != "") $return = trim(strtoupper($data));
+					break;
+				case "DATE":
+					if (trim($data) != ""){
+						$arrdate = explode("-",$data);
+						$return = $arrdate[2]."-".$arrdate[1]."-".$arrdate[0];
+					}
+				break;
+				case "DATE-S":
+					if (trim($data) != ""){
+						$return = substr($data,0,4)."-".substr($data,4,2)."-".substr($data,6,2);
+						if(substr($data,8,6)!=""){
+							$return .= " ".substr($data,8,2).":".substr($data,10,2).":".substr($data,12,2);
+						}
+					}
+				break;
+				case "DATETIME":
+					if (trim($data) != ""){
+						$arrdatetime = explode(" ",$data);
+						if($arrdatetime[1]!="") $time = " ".$arrdatetime[1];
+						$arrdate = explode("-",$arrdatetime[0]);
+						$return = $arrdate[2]."-".$arrdate[1]."-".$arrdate[0].$time;
+					}
+				break;
+			}
 		}
+		return $return;
+	}
+}
+
+if(!function_exists('set_setting')){
+	function set_setting($type=""){
+		$content = "";
+		$data = array();
+		$ci =& get_instance();
+		$ci->load->database();
+		$status = 'N';
+		$KD_ORG = $ci->session->userdata('KD_ORGANISASI');
+		$KD_GROUP = $ci->session->userdata('KD_GROUP');
+		if($KD_GROUP=="SPA") 
+			$status = 'Y';
+		$SQL = "SELECT KD_STATUS FROM app_setting
+				WHERE KD_ORG_SENDER = ".$ci->db->escape($KD_ORG)."
+				AND KD_APRF = ".$ci->db->escape($type);
+		$result = $ci->db->query($SQL);
+		if($result->num_rows() > 0){
+			$row = $result->row();
+			$status = $row->KD_STATUS;
+		}
+		return $status;
+	}
+}
+
+if (!function_exists('name_date')){
+	function name_date($vardate){
+		$pecah1 = explode("-", $vardate);
+		$tanggal = intval($pecah1[0]);
+		$arrayBulan = array("", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli",
+							"Agustus", "September", "Oktober", "November", "Desember");
+		$bulan = $arrayBulan[intval($pecah1[1])];
+		$tahun = intval($pecah1[2]);
+		$balik = $tanggal." ".$bulan." ".$tahun;
+		return $balik;
+	}
+}
+
+if(!function_exists('date_input')){
+	function date_input($vardate,$type=""){
+		$return = "";
+		if (trim($vardate) != ""){
+			$arrdatetime = explode(" ",$vardate);
+			if($arrdatetime!="")
+				$time = " ".$arrdatetime[1];
+			if($type=="auto"){
+				$time = " ".date('H:i:s');
+			}
+			$arrdate = explode("-",$arrdatetime[0]);
+			$return = $arrdate[2]."-".$arrdate[1]."-".$arrdate[0].$time;
+		}
+		return $return;
 	}
 }
 
