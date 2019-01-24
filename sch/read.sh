edf230f26d9bcaa3c6d742d045a33f85b06a50b7
joni.i7 <jhoey.potter7@gmail.com>
2019-01-24 17:39:17 +0700

    Perubahan Read Permit

--- sch/read.sh
+++ sch/read.sh
@@ -1,4 +1,4 @@
 cd /home/elwilis/tps/services
 #php -q index.php/send_laporan
-wget -O - http://103.84.194.194/tps/services.php/scheduler/get_permit/getimpor_sppb --no-check-certificate
-wget -O - http://103.84.194.194/tps/services.php/scheduler/read_permit/sppb --no-check-certificate
+wget -O - http://103.84.194.194/tps_client/index.php/scheduler/get_permit/getimpor_sppb --no-check-certificate
+wget -O - http://103.84.194.194/tps_client/index.php/scheduler/read_permit/sppb --no-check-certificate
